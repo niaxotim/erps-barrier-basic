8404c3408c0dc8b4aec451b604c154e138c6e293
Tim <58301440+niaxotim@users.noreply.github.com>
2023-01-25 13:42:25 +0000

    Removed sounds

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Long, 1)
     Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Up)
 })
 input.onButtonPressed(Button.B, function () {
-    Kitronik_ACCESSbit.buzzerControl(Kitronik_ACCESSbit.BuzzerLength.Short, 5)
     Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
 })
 Kitronik_ACCESSbit.barrierControl(Kitronik_ACCESSbit.BarrierPosition.Down)
